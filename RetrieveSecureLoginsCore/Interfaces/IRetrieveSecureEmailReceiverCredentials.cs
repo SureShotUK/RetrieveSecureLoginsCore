fef0edea8e9123212b72f9463b7b7d2935363268
SureShotUK <irwin455@hotmail.com>
2020-02-21 09:51:31 +0000

    Added support for Email Receiver credentials

---
+++ RetrieveSecureLoginsCore/Interfaces/IRetrieveSecureEmailReceiverCredentials.cs
@@ -0,0 +1,11 @@
+﻿using RetrieveSecureLoginsCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetrieveSecureLoginsCore.Interfaces
+{
+    interface IRetrieveSecureEmailReceiverCredentials : IRetrieveSecureCredentials<EmailReceiver>
+    {
+    }
+}
---
+++ RetrieveSecureLoginsCore/Models/EmailReceiver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetrieveSecureLoginsCore.Models
+{
+    class EmailReceiver
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
